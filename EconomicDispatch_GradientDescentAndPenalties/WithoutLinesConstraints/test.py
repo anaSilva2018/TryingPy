be35a730a90b8c076f4db49876706e7e10eb366d
Ana Silva <36481381+anaSilva2018@users.noreply.github.com>
2020-09-24 23:37:18 +0100

    Update test.py

--- EconomicDispatch_GradientDescentAndPenalties/WithoutLinesConstraints/test.py
+++ EconomicDispatch_GradientDescentAndPenalties/WithoutLinesConstraints/test.py
@@ -4,18 +4,19 @@ Created on Sat Sep 19 14:55:37 2020
 
 @author: Ana Silva
 """
+
 import numpy as np
 from Functions.descgradient import _calcgrad
 from Functions.initialisevalues import _iniatilise
 #                    Gen_i  Bus_i  a(€/h)       b(€/MWh)       c(€/(MW)^2.h)   Emissions(kgCO2/MWh)     Pgmin(MW)   Pgmax(MW)       
 data_gen = np.matrix([[1, 1, 800, 20, 0.06, 80, 30, 90],
                       [2, 1, 800, 16, 0.1, 200, 20, 60],
-                      [3, 4, 800, 20, 0.08, 175, 40, 80], 
+                      [3, 4, 800, 20, 0.08, 175, 40, 80],
                       [4, 4, 800, 22, 0.11, 60, 30, 70]])
-#                      Bus_i Bus_j  Smax(MVA)   
+#                      Bus_i Bus_j  Smax(MVA)
 data_lines = np.matrix([[1, 2, 62],
                         [1, 3, 70],
-                        [1, 5, 45], 
+                        [1, 5, 45],
                         [2, 3, 100],
                         [3, 4, 77],
                         [4, 5, 97]])
@@ -29,15 +30,18 @@ MPar = _DataPar(250, 0.95, 0.005)
 print("******Economical Dispatch*******")
 print(f"Case Study-> Load Power: {MPar.pload} MW Lambda: {MPar.nlamb} LearningRate: {MPar.learnrate}\n")
 [auxcinit, ctext] = _iniatilise(data_gen, MPar)
-cGrad = _calcgrad(MPar, data_gen, auxcinit)
-print(f"PgValues(MW)\n {cGrad.mpgen}\n")
-print(f"GradientValues\n {cGrad.mgradient}\n")
-print(f"TOTAL PENALTY: {cGrad.totpen} €\n")    
-np.savetxt('Outputs/Pgvalues.csv', cGrad.mpgen, delimiter=",", header="Load_Dispatch_(MW)", footer="Rows:"+ctext.tpg)
-np.savetxt('Outputs/MGradient.csv', cGrad.mgradient, delimiter=",", header="Gradient_Matrix", footer="Rows:"+ctext.tpg)
-np.savetxt('Outputs/Totalpenalty.csv', cGrad.mpenalis, delimiter=",", header="Penalties(€)", footer="Rows:"+ctext.tcst)
+[ccost, cval] = _calcgrad(MPar, data_gen, auxcinit)
+print(f"PgValues(MW)\n {cval.mpval}\n")
+print(f"GradientValues\n {cval.mgrad}\n")
+print(f"TOTAL PENALTY: {ccost.ntotalpen} €\n")
+text_file = open("Outputs/totalcost.txt", "w")
+text_file.write(f"Totalcost: {ccost.ntotalcost:.2f} €/ GenerationCost {ccost.cprod:.2f} € /Emissions {ccost.cemis:.2f} €/ Penalties {ccost.ntotalpen:.2f} €")
+text_file.close()
+np.savetxt('Outputs/Pgvalues.csv', cval.mpval, delimiter=",", header="Load_Dispatch_(MW)", footer="Rows:"+ctext.tpg)
+np.savetxt('Outputs/MGradient.csv', cval.mgrad, delimiter=",", header="Gradient_Matrix", footer="Rows:"+ctext.tpg)
+np.savetxt('Outputs/Totalpenalty.csv', ccost.mpen, delimiter=",", header="Penalties(€)", footer="Rows:"+ctext.tcst)
 del(ctext.tpg, ctext.tcst, ctext)
-del(cGrad.mpgen, cGrad.mgradient, cGrad.mpenalis, cGrad.totpen, cGrad)
+del(cval.mpval, cval.mgrad, ccost.mpen, ccost.ntotalpen, ccost, cval)
 del(auxcinit.mpgval, auxcinit.mpgconst, auxcinit.newload, auxcinit.ngen, auxcinit)
 del(MPar.nlamb, MPar.pload, MPar.learnrate)
 del(MPar, data_gen, data_lines)
