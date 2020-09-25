74944bfd7a1f03f608e5005006419a67e95edcbb
Ana Silva <36481381+anaSilva2018@users.noreply.github.com>
2020-09-25 15:19:30 +0100

    Update buses.py

--- FuzzyPowerFlowAC/Functions/buses.py
+++ FuzzyPowerFlowAC/Functions/buses.py
@@ -1,36 +1,42 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 12 12:45:51 2020
-
 @author: Ana Silva
 """
 import numpy as np
-
-def iniciateBuses(data_bus,bus):
+def _iniciateBuses(data_bus):
+    bus = np.size(data_bus, axis=0)
     nQ = 0
     nP = 0
-    busP = np.zeros([1,1])
-    busQ = np.zeros([1,1])
+    busP = np.zeros([1, 1])
+    busQ = np.zeros([1, 1])
     for i in range(bus):
-        if(data_bus[i,1] != 1):
+        if(data_bus[i, 1] != 1):
         #busPQ
-            if(data_bus[i,1] == 2): 
+            if(data_bus[i, 1] == 2):
                 nP = nP+1
-                busP.resize(1,nP)
-                busP[0,nP-1] = data_bus[i,0]
-                
+                busP.resize(1, nP)
+                busP[0, nP-1] = data_bus[i, 0]
                 nQ = nQ+1
-                busQ.resize(1,nQ)
-                busQ[0,nQ-1] = data_bus[i,0]
+                busQ.resize(1, nQ)
+                busQ[0, nQ-1] = data_bus[i, 0]
         #busPV
-            elif(data_bus[i,1] == 3):
+            elif(data_bus[i, 1] == 3):
                 nP = nP+1
-                busP.resize(1,nP)
-                busP[0,nP-1] = data_bus[i,0]
+                busP.resize(1, nP)
+                busP[0, nP-1] = data_bus[i, 0]
         #busRef
         else:
-            busref = int(data_bus[i,0])
-    nzcent = np.size(busP,1)+np.size(busQ,1)
-    
-    return busP, busQ, busref, nP, nQ, nzcent
+            busref = int(data_bus[i, 0])
+    nzcent = np.size(busP, 1)+np.size(busQ, 1)
+    class _Cbus:
+        def __init__(self, mbusP, mbusQ, nbusref, nP, nQ, nz, tbus):
+            self.mbusP = mbusP
+            self.mbusQ = mbusQ
+            self.nbusref = nbusref
+            self.nP = nP
+            self.nQ = nQ
+            self.nz = nz
+            self.tbus = tbus
+    cauxbus = _Cbus(busP, busQ, busref, nP, nQ, nzcent, bus)
+    return cauxbus
     
