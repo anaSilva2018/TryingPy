95f88167c781fa3d28bf2db07e6010e0aab3add7
Ana Silva <36481381+anaSilva2018@users.noreply.github.com>
2020-09-25 13:17:20 +0100

    Update buses.py

--- FuzzyPowerFlowDC/Functions/buses.py
+++ FuzzyPowerFlowDC/Functions/buses.py
@@ -1,31 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 12 12:45:51 2020
-
 @author: Ana Silva
 """
 import numpy as np
-
-def iniciateBuses(data_bus,bus):
+def _iniciateBuses(data_bus):
     nP = 0
-    busP = np.zeros([1,1])
+    busP = np.zeros([1, 1])
     text_theta = ""
     text_Pinj = ""
+    bus = np.size(data_bus, axis=0)
     for i in range(bus):
-        if(data_bus[i,1] != 1):
-        #busPQ
+        if(data_bus[i, 1] != 1):
+        #busP
             nP = nP+1
-            busP.resize(1,nP)
-            busP[0,nP-1] = data_bus[i,0]
-            
-            text_theta = text_theta + "_theta" + str(int(data_bus[i,0]))
-            text_Pinj = text_Pinj + "_P" + str(int(data_bus[i,0]))
-
+            busP.resize(1, nP)
+            busP[0, nP-1] = data_bus[i, 0]
+            text_theta = text_theta + "_theta" + str(int(data_bus[i, 0]))
+            text_Pinj = text_Pinj + "_P" + str(int(data_bus[i, 0]))
         else:
-            busref = int(data_bus[i,0])
-    nzcent = np.size(busP,1)
+            busref = int(data_bus[i, 0])
+    nzcent = np.size(busP, 1)
+    text_theta = "Rows:" + text_theta
+    text_Pinj = "Rows:" + text_Pinj
+    class _Cbus:
+        def __init__(self, mbusp, nbusref, nP, nz, ttheta, tpinj, tbus):
+            self.mbusp = mbusp
+            self.nbusref = nbusref
+            self.nP = nP
+            self.nz = nz
+            self.ttheta = ttheta
+            self.tpinj = tpinj
+            self.tbus = tbus
+    cauxb = _Cbus(busP, busref, nP, nzcent, text_theta, text_Pinj, bus)
+    return cauxb
     
-    text_theta = "Rows:" + text_theta 
-    text_Pinj = "Rows:" + text_Pinj 
-
-    return busP, busref, nP, nzcent, text_theta, text_Pinj
